0a5157160a24c008aff4d1e97cc748cadd9a4e10
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2020-11-06 13:32:41 +0530

    Update ec2.tf

--- provisioning/remote-exec-with-script-file/ec2.tf
+++ provisioning/remote-exec-with-script-file/ec2.tf
@@ -3,13 +3,15 @@ ami =var.ami
 instance_type= var.type
 key_name = var.key
 vpc_security_group_ids = [aws_security_group.remote-sg.id]
+ count=2
 }
 resource "null_resource" "remot" {
+ count=2
 connection {
 type="ssh"
 user="ubuntu"
 private_key = file("./ShyamKey.pem")
-host = aws_instance.myec2.public_ip
+host = aws_instance.myec2[count.index].public_ip
 }
 provisioner "file" {
 source = "./script.sh"
