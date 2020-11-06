21e90b71f3ebe8f0bc2b77c3647bd0d29fa06572
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2020-11-06 13:26:21 +0530

    Create ec2.tf

---
+++ provisioning/remote-exec-with-file1/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "myec2" {
+ami =var.ami
+instance_type= var.type
+key_name = var.key
+vpc_security_group_ids = [aws_security_group.remote-sg.id]
+count=2
+connection {
+type="ssh"
+user="ubuntu"
+private_key = file("./ShyamKey.pem")
+host = self.public_ip
+}
+provisioner "file" {
+source = "./script.sh"
+destination = "/home/ubuntu/script"
+}
+provisioner "remote-exec" {
+inline = [
+"chmod +x /home/ubuntu/script",
+ "sudo /home/ubuntu/script"
